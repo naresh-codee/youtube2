d1e1e900c1cf55d72add684ce52c01b82853689b
naresh-codee <githubnaresh@gmail.com>
2024-07-23 18:43:05 +0530

    sidebar done

--- src/components/maincontainer.js
+++ src/components/maincontainer.js
@@ -4,7 +4,7 @@ import Videocontainer from "./videocontainer";
 
 const Maincontainer = () =>{
     return(
-        <div>
+        <div className="flex">
             <Buttonlist/>
             <Videocontainer/>
         </div>
