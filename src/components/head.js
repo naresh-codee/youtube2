1fe94cadd5bbfde189ba9885e70921c4a3555445
naresh-codee <githubnaresh@gmail.com>
2024-07-24 15:05:00 +0530

    little styling

--- src/components/head.js
+++ src/components/head.js
@@ -20,21 +20,21 @@ const Head = () => {
     return(
         <div className="grid grid-flow-col items-center my-1 shadow-lg">
             <div className="flex p-1 col-span-1 items-center ">
-                <img className="h-10 w-12" alt="hamburger_menu" src={hamburgermenu} onClick={() =>{
+                <img className="h-10 w-12 cursor-pointer" alt="hamburger_menu" src={hamburgermenu} onClick={() =>{
                     hamburgerMenuToggle()
                 }} />
-                <img className="h-16 w-24" alt="youtube_logo" src={youtube_logo}/>
+                <img className="h-16 w-24 cursor-pointer" alt="youtube_logo" src={youtube_logo}/>
             </div>
 
             <div className="col-span-10">
                 
-                <input className=" border border-black border-r-none rounded-full rounded-r-none py-2 px-2 w-1/2 bg-slate-300 text-black " type="text" placeholder="Search" />
-                <button className="border border-black border-l-transparent rounded-r-full p-2 w-10">🔍</button>
+                <input className=" border border-black border-r-none rounded-full rounded-r-none py-2 px-2 w-1/2 bg-slate-300 text-black cursor-pointer " type="text" placeholder="Search" />
+                <button className="border border-black border-l-transparent rounded-r-full p-2 w-10 cursor-pointer">🔍</button>
             </div>
 
 
             <div>
-                <img className="col-span-1 h-12 w-13  p-1 " alt="user_logo" src={user_logo}/>
+                <img className="col-span-1 h-12 w-13  p-1 cursor-pointer" alt="user_logo" src={user_logo}/>
             </div>
         </div>
     )
