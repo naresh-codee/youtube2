ce73e5884ea4c3d5a2bd0d984b86621bb53acc58
naresh-codee <githubnaresh@gmail.com>
2024-07-24 18:24:08 +0530

    button list done

--- src/components/buttonlist.js
+++ src/components/buttonlist.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 const Buttonlist = () =>{
+    const buttons = ["javascript","tamil cinema","Music","News","Gaming","Mixes","live","software development","Javascript","science fiction" ,"pop music","Engineering"]
     return (
-        <div>
-            button list
+        <div className="flex">
+            {buttons.map((b,index) =>(
+               
+                    <button key={index} className= " bg-slate-300 m-1 py-1 px-2 border border-black shadow-lg rounded-lg " >{b}</button>
+                
+                
+            ))
+            }
         </div>
     )
 }
