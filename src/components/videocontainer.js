e16397f324c26c3b8d7516fa5339cf248d62e3d6
naresh-codee <githubnaresh@gmail.com>
2024-07-24 19:25:34 +0530

    getting 50 video results by adding &maxResults=50 in the youtube_api variable

--- src/components/videocontainer.js
+++ src/components/videocontainer.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Videocard from "./videocards";
 
+import { useEffect,useState } from "react";
+import youtube_api from "../constants/urls";
+
 const Videocontainer = () =>{
+
+    const [video,setVideo] = useState([]);
+    useEffect(()=> {
+        getVideos()
+    },[]);
+
+    const getVideos = async () =>{
+        const data = await fetch(youtube_api);
+        const jsonValue = await data.json();
+        console.log(jsonValue.items);
+        setVideo(jsonValue.items);
+        
+
+    }
+
     return(
         <div>
+
             <Videocard/>
         </div>
     )
