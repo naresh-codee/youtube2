6030e81ec44778a068eb562ad63d5be32ea94836
naresh-codee <githubnaresh@gmail.com>
2024-07-25 13:59:45 +0530

    STEP2: creating the routing configuration

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ import Body from "./components/body";
 
 import {Provider} from "react-redux";
 import store from "../src/utils/store";
+ 
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import SignIn from "./components/signin";
+import Contactus from "./components/contactus";
+
+const AppRouting = createBrowserRouter([
+  {
+    path:"/",
+    element:<Body/>
+  },
+  {
+    path:"/signin",
+    element:<SignIn/>
+  },
+  {
+    Path:"/contact",
+    element:<Contactus/>
+  },
+]);
+
 function App() {
   return (
     <Provider store={store}>
